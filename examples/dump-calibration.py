f4bf1a3126fc592e77fd176a89c539a3d23300a4
Sandy Macdonald <sandyjmacdonald@gmail.com>
2019-06-09 12:41:50 +0100

    Tidying up and adding new examples

--- examples/dump-calibration.py
+++ examples/dump-calibration.py
@@ -1,12 +1,20 @@
+#!/usr/bin/env python
+
+from bmp280 import BMP280
+
 try:
     from smbus2 import SMBus
 except ImportError:
     from smbus import SMBus
-from bmp280 import BMP280
 
+print("""dump-calibration.py - Dumps calibration data.
 
+Press Ctrl+C to exit!
+
+""")
+
+# Initialise the BMP280
 bmp280 = BMP280(i2c_dev=SMBus(1))
-
 bmp280.setup()
 
 for key in dir(bmp280.calibration):
--- examples/temperature-and-pressure.py
+++ examples/temperature-and-pressure.py
@@ -1,11 +1,20 @@
+#!/usr/bin/env python
+
 import time
+from bmp280 import BMP280
+
 try:
     from smbus2 import SMBus
 except ImportError:
     from smbus import SMBus
-from bmp280 import BMP280
 
+print("""temperature-and-pressure.py - Displays the temperature and pressure.
 
+Press Ctrl+C to exit!
+
+""")
+
+# Initialise the BMP280
 bus = SMBus(1)
 bmp280 = BMP280(i2c_dev=bus)
 
